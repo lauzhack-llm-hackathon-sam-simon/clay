e42fb4692c17a847f7e9a9e821f6492cb9a58993
Androz <androz2091@gmail.com>
2025-05-16 23:31:48 +0200

    play more with pixijs

---
+++ frontend/cray-frontend/src/app/_app.js
@@ -0,0 +1,9 @@
+import { PrimeReactProvider } from 'primereact/api';
+
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <PrimeReactProvider>
+            <Component {...pageProps} />
+        </PrimeReactProvider>
+    );
+}
