d6384b771180c853e3a042e699d2afdb4be4b7ad
Androz <androz2091@gmail.com>
2025-05-16 23:23:04 +0200

    init splitters

--- frontend/cray-frontend/src/app/page.js
+++ frontend/cray-frontend/src/app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import { Splitter, SplitterPanel } from "primereact/splitter";
+import PixiCanvas from "./pixi-canvas";
 
 export default function Home() {
   return (
     <Splitter style={{ height: '90vh' }}>
-        <SplitterPanel className="flex align-items-center justify-content-center">Panel 1</SplitterPanel>
-        <SplitterPanel className="flex align-items-center justify-content-center">Panel 2</SplitterPanel>
+        <SplitterPanel className="flex align-items-center justify-content-center" size={70}>
+          <PixiCanvas />
+        </SplitterPanel>
+        <SplitterPanel className="flex align-items-center justify-content-center" size={30}>Panel 2</SplitterPanel>
     </Splitter>
   );
 }
