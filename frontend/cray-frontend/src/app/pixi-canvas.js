9bd50270c0035e0f251c7a8d4e0a36ae9ad9a3b7
Androz <androz2091@gmail.com>
2025-05-16 23:57:12 +0200

    now scrollable! yay

--- frontend/cray-frontend/src/app/pixi-canvas.js
+++ frontend/cray-frontend/src/app/pixi-canvas.js
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef } from 'react';
 import * as PIXI from 'pixi.js';
+import { Viewport } from 'pixi-viewport';
 
 const PixiCanvas = () => {
   const pixiContainerRef = useRef(null);
@@ -17,6 +18,20 @@ const PixiCanvas = () => {
 
       pixiContainerRef.current.appendChild(app.canvas);
 
+      const viewport = new Viewport({
+          screenWidth: pixiContainerRef.current.offsetWidth,
+          screenHeight: pixiContainerRef.current.offsetHeight,
+          worldWidth: 1000,
+          worldHeight: 1000,
+          events: app.renderer.events, // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+      });
+
+      // add the viewport to the stage
+      app.stage.addChild(viewport);
+
+      // activate plugins
+      viewport.drag().pinch().wheel();
+
       // Load the bunny texture.
       const texture = await PIXI.Assets.load({
         src: "https://www.gravatar.com/avatar/2c7d99fe281ecd3bcd65ab915bac6dd5?s=250",
@@ -28,19 +43,17 @@ const PixiCanvas = () => {
 
       const circle = new PIXI.Graphics()
       .circle(0, 0, 100)
-      .fill(0xFF0000);
+      .fill(texture);
 
-      app.stage.addChild(circle);
-
-      // Add to stage
-      app.stage.addChild(bunny);
+      viewport.addChild(circle);
 
       // Center the sprite's anchor point
       bunny.anchor.set(0.5);
 
       // Move the sprite to the center of the screen
-      bunny.x = app.screen.width / 2;
-      bunny.y = app.screen.height / 2;
+      circle.x = app.screen.width / 2;
+      circle.y = app.screen.height / 2;
+
     }
     loadPixi();
   }, []);
